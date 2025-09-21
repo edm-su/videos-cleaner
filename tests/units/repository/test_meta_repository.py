ddd8a83cdf735a489801368d05b1d85ce373c0e5
Egor Glukhikh <eagluhih@gmail.com>
2025-09-21 16:16:30 +0500

    fix: unauthorized error raised for meta repository

--- tests/units/repository/test_meta_repository.py
+++ tests/units/repository/test_meta_repository.py
@@ -9,7 +9,10 @@ from videos_cleaner.adapters.repositories.meta_repository import (
     MetaRepostiory,
     YoutubeDataApiRepository,
 )
-from videos_cleaner.domain.interfaces.meta_repository import ExistsStatus
+from videos_cleaner.domain.interfaces.meta_repository import (
+    ExistsStatus,
+    UnauthorizedError,
+)
 
 pytestmark = pytest.mark.anyio
 
@@ -60,6 +63,18 @@ class TestMetaRepository:
 
         assert route.called
         assert result == ExistsStatus.REMOVED
+
+    @respx.mock
+    async def test_unauthorized(self, repo: MetaRepostiory) -> None:
+        route = respx.head(
+            "https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=test"
+        )
+        route.return_value = Response(401)
+
+        with pytest.raises(UnauthorizedError):
+            _ = await repo.is_exists("test")
+
+        assert route.called
 
 
 class TestYouTubeDataAPIRepository:
